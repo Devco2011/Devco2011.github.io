f00382d65dc771c59dae8e10420a5f61af6f5ec3
Devco2011 <devco2011@gmail.com>
2021-01-10 10:15:24 -0600

    tidy family in work section complete

--- src/main/Work.js
+++ src/main/Work.js
@@ -9,22 +9,23 @@ export const Work = () => {
             <Container id="work-container" fluid={true}>
                 <Row className="justify-content-md-center">
                     <Col md={10}>
-                        <h3 className="section-title">MY WORK</h3>
+                        <h3 className="section-title">CASE STUDIES</h3>
                     </Col>
                     <Col md={5}>
                         <div className="images">
-                            <img className="tidyFamHome" src="../../images/FamHome.jpg" alt="Homepage view of Tidy Family web app." />
+                            <a href="#work"><img className="tidyFamHome" src="../../images/FamHome.jpg" alt="Homepage view of Tidy Family web app." /></a>
                         </div>
                     </Col>
                     <Col md={5}>
                         <div className="work-div">
-                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><h6>Tidy Family: A Project of Firsts</h6></ScrollAnimation>
-                            <li>My first react app</li>
-                            <li>My first experience with user research</li>
-                            <li>My first user interviews</li>
-                            <li>My first solo project</li>
-                            <li>My first case study</li>
-                            <p><a href="#work"><u><strong>View the case study</strong></u></a></p>
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><h5 className="section-title">Tidy Family: A Project of Firsts</h5></ScrollAnimation>
+                            <p><li>My first react app</li>
+                                <li>My first experience with user research</li>
+                                <li>My first user interviews</li>
+                                <li>My first solo project</li>
+                                <li>My first case study</li>
+                            </p>
+                            <p><a href="#work"><strong>View the case study</strong></a></p>
                         </div>
 
                     </Col>
