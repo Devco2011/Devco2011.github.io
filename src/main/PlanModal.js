297ddb1f1e5bd498f57f2c3dc704c4112e975c58
Devco2011 <devco2011@gmail.com>
2021-01-09 21:17:05 -0600

    plan modal

---
+++ src/main/PlanModal.js
@@ -0,0 +1,32 @@
+import { React, useState } from 'react';
+import { Modal, Nav } from 'react-bootstrap';
+
+export const PlanModal = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+
+            <Nav.Link onClick={handleShow}>Here's an expample in my work.</Nav.Link>
+
+
+            <Modal show={show} onHide={handleClose} dialogClassName={"primaryModal"}>
+                <Modal.Header closeButton>
+                    <Modal.Title className="modalTitle">TIDY FAMILY WEB APP</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="modalBody"><p>Tidy family was my front-end capstone. I used visuals every step of the way.</p>
+                    <li>Pencil and paper wireframe for first user interviews</li>
+                    <li>Pencil and paper wireframe for V2, after initial interviews</li>
+                    <li>Whiteboard wireframe for first user flow design</li>
+                    <li> Figma mockup to solidify design elements</li>
+                    <p><Nav.Link>
+                        Here's an expample in my work.
+                      </Nav.Link></p>
+                </Modal.Body>
+            </Modal>
+        </>
+    );
+}
