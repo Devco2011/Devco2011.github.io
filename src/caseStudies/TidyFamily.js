3d9def95ab3dba8d606eb586d5aeadde76df3af1
Devco2011 <devco2011@gmail.com>
2021-01-11 17:25:27 -0600

    saving case study work

--- src/caseStudies/TidyFamily.js
+++ src/caseStudies/TidyFamily.js
@@ -13,7 +13,25 @@ export const TFCaseStudy = () => {
                     <Col md={12}>
                         <h3 className="section-title">TESTING NEW KNOWLEDGE</h3>
                     </Col>
-
+                </Row>
+                <Row className="justify-content-md-center mt-5">
+                    <Col md={8} className="mt-4">
+                        <div className="boxBorder">
+                            <div className="knowledgeBox"><p className="whatILearnedText"><strong>Three months into my bootcamp at Nashville Software School, I was given an assignment to build my
+                                first React app from the ground up, and I had just two weeks to reach MVP.</strong></p><br></br>
+                                <p className="whatILearnedText"><strong>The primary goal of this project was to assess my proficiency with my new skills in JavaScript, React, JSON, HTML, and CSS.</strong></p></div>
+                        </div>
+                    </Col>
+                </Row>
+                <Row className="justify-content-md-center pt-5">
+                    <Col md={8} className="mt-4">
+                        <div className="csImages">
+                            <img className="tidyFamHomeCS" src="../../images/FamHome.jpg" alt="Homepage view of Tidy Family web app." />
+                        </div>
+                    </Col>
+                    <Col md={8} className="mt-5">
+                        <h5 className="section-title">FRAMING THE CHALLENGE</h5>
+                    </Col>
                 </Row>
 
             </Container>
