d578be7dc173ce57accdf836523976f4fa2d4016
Devco2011 <devco2011@gmail.com>
2021-01-12 21:01:38 -0600

    more CSS work

--- src/main/Work.js
+++ src/main/Work.js
@@ -12,26 +12,24 @@ export const Work = () => {
             <Container id="light-blue-container" fluid>
                 <Row className="justify-content-md-center">
                     <Col className="col-12-md-6">
-                        <h3 className="section-title">My Work</h3>
+                        <h3 className="section-title pb-5">My Work</h3>
                     </Col>
                 </Row>
                 <Row className="justify-content-md-center">
-                    <Col className="col-12 col-md-4">
-                        <div className="images">
-                            <Link to="/CaseStudies/TidyFamily" target="blank"><img className="tidyFamHome" src="../../images/FamHome.jpg" alt="Homepage view of Tidy Family web app." /></Link>
+                    <Col className="col-12 col-md-5">
+                        <div className="images mt-3">
+                            <Link to="/CaseStudies/TidyFamily" target="blank"><img className="tidyFamHome" src="../../images/FamHome.jpg" height="350em" width="390em" alt="Homepage view of Tidy Family web app." /></Link>
                         </div>
                     </Col>
-                    <Col className="col-12 col-md-4">
-                        <div className="work-div">
-                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><h5 className="section-title">Tidy Family: A Project of Firsts</h5></ScrollAnimation>
-                            <p><li>My first react app</li>
-                                <li>My first user research</li>
-                                <li>My first user interviews</li>
-                                <li>My first solo project</li>
-                                <li>My first case study</li>
-                            </p>
-                            <p><Link to="/CaseStudies/TidyFamily" target="blank"><strong>View the case study</strong></Link></p>
-                        </div>
+                    <Col className="col-12 col-md-5">
+                        <ScrollAnimation animateIn="animate__animated animate__fadeIn"><h5 className="section-title mt-2">Tidy Family: A Project of Firsts</h5></ScrollAnimation>
+                        <p><li>My first react app</li>
+                            <li>My first user research</li>
+                            <li>My first user interviews</li>
+                            <li>My first solo project</li>
+                            <li>My first case study</li>
+                        </p>
+                        <p><Link to="/CaseStudies/TidyFamily" target="blank"><strong>View the case study</strong></Link></p>
 
                     </Col>
                 </Row>
