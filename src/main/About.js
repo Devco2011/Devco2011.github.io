f735cfae8fd5ca7f9d402131d321a078dfef3f24
Devco2011 <devco2011@gmail.com>
2021-02-06 12:01:38 -0600

    redesign of about section

--- src/main/About.js
+++ src/main/About.js
@@ -11,38 +11,52 @@ export const About = () => {
                 <div id="about"></div>
                 <Row className="justify-content-md-center">
                     <Col className="col-12 col-md-6 mt-2">
-                        <h3 className="section-title">ABOUT ME</h3>
+                        <div className="knowledgeBox">
+                            <h3 className="section-title">ABOUT ME</h3>
+                        </div>
                     </Col>
                 </Row>
                 <Row className="justify-content-md-center">
-                    <Col className="col-12 col-md-3">
-                        <div className="images">
-                            <img className="stickmanThumbsUp mt-5" src="../../images/stickmanThumbsUp.png" height="200em" width="100em" alt="Sickman giving a thumbs up!" />
-                        </div>
-                    </Col>
 
-                    <Col className="col-12 col-md-5">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
-                            <h5 className="section-title">I love solving problems through design and code.</h5>
-                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>I believe <strong>human-centered</strong> design is important work that has the potential to improve lives.
-                        My understanding of front-end languages enhances my ability to design for the user. I am driven by the desire to do work that makes a difference.</p></ScrollAnimation>
+                            <center><h5 className="section-title">HOW CAN I HELP YOU?</h5></center>
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p className="p-3">When thinking about what would be relevant about my life to share with you here, I came
+                            to the conclusion that it is all relevant. I won’t write my whole story here, but I would love to elaborate when we connect. My life
+                                experience can be an asset to your organization, your team, and your users. </p></ScrollAnimation>
                         </div>
 
                     </Col>
                 </Row>
-                <Row className="justify-content-md-center pt-5">
-                    <Col className="col-12 col-md-3">
-                        <div className="images">
-                            <img className="stickmanBook mt-5" src="../../images/stickmanBook.png" height="210em" width="100em" alt="Sickman reading a book." />
+                <Row className="justify-content-md-center pt-3">
+                    <Col className="col-12 col-md-3 p-4 border-right">
+                        <div>
+                            <h5 className="section-title">EMPATHY</h5>
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>I have looked at life from many different perspectives. I came to the field of technology after
+                            ten years of being a full-time performing singer and songwriter. Before that, I owned a small residential construction business. I have four kids, one is
+                                a cancer survivor. I’ve known struggle, success, and all points in between. </p><p><strong>When it comes to empathy, I have a deep well of experiences to draw from.
+                                </strong></p></ScrollAnimation>
                         </div>
                     </Col>
-                    <Col className="col-12 col-md-5">
-                        <div className="about-div">
-                            <h5 className="section-title">I am always learning.</h5>
-                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>I love to consume information that I can put to work
-                            as a designer and developer. I am passionate about serving the <strong>user</strong>, and that passion fuels my
-                            desire to constantly learn, expand my knowledge, and improve my craft. I will always be a student.
-                            </p></ScrollAnimation>
+                    <Col className="col-12 col-md-3 p-4 border-right">
+                        <div>
+                            <h5 className="section-title">COLLABORATION</h5>
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>Years of songwriting, recording, and performing have taught me the value of everyone in the room being
+                            comfortable enough to put their worst idea on the table. In Nashville, it’s normal for songwriter splits to be equal, even if one writer didn’t write a single line
+                                in the song. That writer’s presence in the room and participation in the process earns them an equal share. I believe in that philosophy.</p>
+
+                                <p><strong>My experience at Nashville Software School taught me how to collaborate remotely. Our cohort was 100% remote, full-time, for six months.
+                                </strong></p></ScrollAnimation>
+                        </div>
+                    </Col>
+                    <Col className="col-12 col-md-3 p-4">
+                        <div>
+                            <h5 className="section-title">PURPOSE</h5>
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>The people I work with and the work itself are most important to me. I know what it’s like to hang, have
+                                fun, and accomplish great things together. </p>
+
+                                <p><strong>The best situation is one where the team is positive and supportive and the work is challenging and rewarding.
+                                </strong></p></ScrollAnimation>
                         </div>
                     </Col>
                 </Row>
