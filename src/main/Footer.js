f0abcfcb5c1476b6cf7fb3032697c601ffcf109f
Devco2011 <devco2011@gmail.com>
2021-02-06 23:03:57 -0600

    footer redesign

--- src/main/Footer.js
+++ src/main/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaGithub, FaLinkedin, FaEnvelope, FaMobileAlt } from 'react-icons/fa'
 import { Nav, Navbar } from "react-bootstrap";
 
 
@@ -8,9 +9,43 @@ export const Footer = () => {
 
         <Navbar expand='lg' variant='dark' bg='dark' sticky='bottom' id='footer'>
             <Nav className='ml-auto mr-auto'>
-                <Navbar.Text id='copyright'>
-                    <p>&copy; {new Date().getFullYear()} Devon Cox</p>
-                </Navbar.Text>
+                <Container className="pt-4">
+                    <Row>
+                        <Col className="col-4">
+                            <Navbar.Text><h6 className="section-title">CONNECT WITH ME</h6>
+                                <ul className="list-unstyled">
+                                    <li><FaMobileAlt /> 615-310-7665</li>
+                                    <li><FaEnvelope /> devco2011@gmail.com</li>
+                                </ul>
+                            </Navbar.Text>
+                        </Col>
+                        <Col className="col-4">
+                            <Navbar.Text><h6 className="section-title">ONLINE</h6>
+                                <ul className="list-unstyled">
+                                    <li><FaLinkedin /> linkedin.com/in/devonhcox</li>
+                                    <li><FaGithub /> github.com/Devco2011</li>
+
+                                </ul>
+                            </Navbar.Text>
+                        </Col>
+                        <Col className="col-4">
+                            <Navbar.Text><h6 className="section-title">TECH/SKILLS USED FOR THIS SITE</h6>
+                                <ul className="list-unstyled">
+                                    <li>React | React Bootstrap |</li>
+                                    <li>Sass | Git | GitHub |</li>
+                                    <li>User Testing | Figma</li>
+
+                                </ul>
+                            </Navbar.Text>
+                        </Col>
+
+                        <Col>
+                            <Navbar.Text>
+                                <p className="lightBlue"> &copy; {new Date().getFullYear()} Devon Cox | All rights reserved.</p>
+                            </Navbar.Text>
+                        </Col>
+                    </Row>
+                </Container>
             </Nav>
         </Navbar>
 
