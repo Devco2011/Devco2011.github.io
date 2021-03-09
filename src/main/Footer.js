a684b418d502b0aa2ef8ea675c60001d5aadaaf4
Devco2011 <devco2011@gmail.com>
2021-03-09 09:15:54 -0600

    links hooked up

--- src/main/Footer.js
+++ src/main/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'react-bootstrap';
 import { FaGithub, FaLinkedin, FaEnvelope, FaMobileAlt } from 'react-icons/fa'
 import { Nav, Navbar } from "react-bootstrap";
@@ -11,24 +12,24 @@ export const Footer = () => {
             <Nav className='ml-auto mr-auto'>
                 <Container className="pt-4">
                     <Row>
-                        <Col className="col-4">
+                        <Col className="col-12 col-md-4">
                             <Navbar.Text><h6 className="section-title">CONNECT WITH ME</h6>
                                 <ul className="list-unstyled">
-                                    <li><FaMobileAlt /> 615-310-7665</li>
-                                    <li><FaEnvelope /> devoncoxdev@gmail.com</li>
+                                    <li><FaMobileAlt /> <a href="tel:615-310-7665" target="_blank" rel="noreferrer">615-310-7665</a></li>
+                                    <li><FaEnvelope /> <a href="mailto:devcoxdev@gmail.com" target="_blank" rel="noreferrer">devoncoxdev@gmail.com</a></li>
                                 </ul>
                             </Navbar.Text>
                         </Col>
-                        <Col className="col-4">
+                        <Col className="col-12 col-md-4">
                             <Navbar.Text><h6 className="section-title">ONLINE</h6>
                                 <ul className="list-unstyled">
-                                    <li><FaLinkedin /> linkedin.com/in/devonhcox</li>
-                                    <li><FaGithub /> github.com/Devco2011</li>
+                                    <li><FaLinkedin /><a href="https://linkedin.com/in/devonhcox" target="_blank" rel="noreferrer"> linkedin.com/in/devonhcox</a></li>
+                                    <li><FaGithub /><a href="https://github.com/Devco2011" target="_blank" rel="noreferrer"> github.com/Devco2011</a></li>
 
                                 </ul>
                             </Navbar.Text>
                         </Col>
-                        <Col className="col-4">
+                        <Col className="col-12 col-md-4">
                             <Navbar.Text><h6 className="section-title">TECH/SKILLS USED FOR THIS SITE</h6>
                                 <ul className="list-unstyled">
                                     <li>React | React Bootstrap |</li>
