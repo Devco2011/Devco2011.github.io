9e16d61a8c72aa485cf3d4ae53dea7bc92304d7a
Devco2011 <devco2011@gmail.com>
2021-03-08 21:14:21 -0600

    more on work, also resume

--- src/main/Resume.js
+++ src/main/Resume.js
@@ -15,10 +15,10 @@ export const Resume = () => {
                     </Col>
                 </Row>
                 <Row className="justify-content-md-center mt-3">
-                    <Col className="col-12 col-md-4  border-right">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
                             <h5 className="section-title-light">ABOUT ME</h5>
-                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>I am a motivated creative with 6 months of experience in
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>I am a motivated creative with experience in
                             ux/ui design and frontend software development. Prior to
                             becoming a frontend software developer, I was a professional
                             musician. I am personable and an enthusiastic collaborator,
@@ -29,49 +29,54 @@ export const Resume = () => {
                         </div>
                     </Col>
 
-                    <Col className="col-12 col-md-4  pl-4">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
                             <h5 className="section-title-light">SKILLS</h5>
-                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p>User Research, User Testing, UX Design,
-                            Experience Strategy, Interaction Design, Personas, User Interface Design, Journey Mapping, Usability Testing, HTML, JavaScript, Prototyping, Wireframing,
-                                CSS, REACT JS, Sass, Git, GitHub, Json, Firebase</p>
+                            <ScrollAnimation animateIn="animate__animated animate__fadeIn"><p><strong>JavaScript ES6 | HTML5 | CSS3 | Sass | React JS | Vue JS | REST API | User Research | User Testing | UX Design |
+                            Experience Strategy | Interaction Design | Personas | User Interface Design | Journey Mapping | Usability Testing | Prototyping | Wireframing |
+                                Agile | Scrum | Remote | Figma | Adobe CC | JSON | Firebase</strong></p>
                                 <br></br></ScrollAnimation>
                         </div>
                     </Col>
                 </Row>
                 <Row className="justify-content-md-center">
-                    <Col className="col-12 col-md-4 border-right">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
                             <h5 className="section-title-light">TECHNICAL EXPERIENCE</h5>
                             <ScrollAnimation animateIn="animate__animated animate__fadeIn"><h6 className="section-title-light">Nashville Software School</h6>
-
-                                <p>Apprentice<br></br>
-                                August 2020 - February 2021</p>
+                                <p>Software Developer<br></br>
+                                August 2020 - February 2021</p><br></br>
+                                <p className="resume">Studied front-end web development and UI/UX design in a hands-on, learn-by-doing environment.</p>
                                 <ul>
-                                    <li>Intensive, full-time, six month front-end web development and UI/UX design bootcamp</li><br></br>
-                                    <li>Worked individually and on remote teams in an Agile workflow environment</li><br></br>
-                                    <li>Developed the skills and mindset that will enable me to continually learn new technologies</li><br></br>
-                                    <li>100% Remote</li>
+                                    <li>Developed dynamic SPAs using React in addition to modern ES6+ JavaScript</li>
+                                    <li>Learned frontend fundamentals such as HTML5, CSS6, Sass, JSON, modular code, reusable components, and database structure using ERDs and Firebase</li>
+                                    <li>Gained competency in UI/UX design fundamentals such as wire framing, prototyping research, user testing, journey mapping, site mapping, and personas</li>
+                                    <li>Exposed to an Agile environment through frequent group projects including daily stand ups, planning, ticket assignments, and problem solving</li>
                                 </ul>
                                 <br></br></ScrollAnimation>
                         </div>
                     </Col>
-                    <Col className="col-12 col-md-4 pl-4">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
                             <h5 className="section-title-light">PROJECTS</h5>
                             <ScrollAnimation animateIn="animate__animated animate__fadeIn">
-                                <p><strong>Tidy Family <FaGithub /></strong><br></br>
-                                    <i>Front-end Captstone</i></p>
+                                <p><strong>Tidy Family </strong><br></br>
+                                    Front-end Captstone</p>
                                 <ul>
                                     <li>Figma, JavaScript, HTML, CSS, JSON, React, ReactStrap, Git, GitHub</li>
                                 </ul>
-                                <p><strong>Zero <FaGithub /></strong><br></br>
-                                    <i>UI/UX Captstone</i></p>
+                                <p><strong>C43 Class Website</strong><br></br>
+                                    Team Project</p>
+                                <ul>
+                                    <li>JavaScript, HTML, CSS, JSON, React JS, Git, GitHub, User Testing</li>
+                                </ul>
+                                <p><strong>Zero </strong><br></br>
+                                    UI/UX Captstone</p>
                                 <ul>
                                     <li>Figma, JavaScript, HTML, CSS, JSON, React, ReactStrap, Git, GitHub, Firebase</li>
                                 </ul>
                                 <p><strong>TN.gov Redesign</strong><br></br>
-                                    <i>Team Project</i></p>
+                                    Team Project</p>
                                 <ul>
                                     <li>Figma, Remote User Testing, Journey Mapping, Prototyping, Presenting</li>
                                 </ul>
@@ -81,7 +86,7 @@ export const Resume = () => {
 
                 </Row>
                 <Row className="justify-content-md-center">
-                    <Col className="col-12 col-md-4 border-right">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
                             <h5 className="section-title-light">TECHNICAL EXPERIENCE</h5>
                             <ScrollAnimation animateIn="animate__animated animate__fadeIn"><h6 className="section-title-light">Nashville Software School</h6>
@@ -96,7 +101,7 @@ export const Resume = () => {
                                 <br></br></ScrollAnimation>
                         </div>
                     </Col>
-                    <Col className="col-12 col-md-4 pl-4">
+                    <Col className="col-12 col-md-8">
                         <div className="about-div">
                             <h5 className="section-title-light">PROJECTS</h5>
                             <ScrollAnimation animateIn="animate__animated animate__fadeIn">
