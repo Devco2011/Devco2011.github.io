4a5690a2890953e94bf4c67759dbc2cf4d953cd4
Devco2011 <devco2011@gmail.com>
2023-11-08 10:40:55 -0600

    added LFP

--- src/main/Work.js
+++ src/main/Work.js
@@ -36,30 +36,38 @@ export const Work = () => {
                                 <li className='mb-2' class="lightBlue"><p class="lightBlue">I helped develop an application to streamline a business services organization's resource management. This application gives project managers access to resource availablity, location, rate, etc. It also provides executives insight into utilization. As part of the team that designed the application, I participated in UX research including interviews with stakeholders. </p></li>
                             </ul>
                         </div>
-                        
-                        <h6 className="section-title mt-4">DocuSign CLM Developer Capstone</h6>
+                        <h6 className="section-title mt-4">Integrations Consultant/Developer</h6> 
+                        <div className="images mt-3">
+                        <ul>
+                               <li className='mb-2' class="lightBlue"><p class="lightBlue">I worked on a team helping to automate and improve procurement processes for one of the world's largest snack companies, through an integration with their homegrown systems and DocuSign CLM. Acions in the source system trigger an API request, which kicks off workflows in DocuSign CLM. These workflows receive data from the source system via the API request, then validate and prepare that data for various uses within DocuSign CLM, including contracting and reporting. Status updates and other data are sent back to the source system via API requests. I also designed and developed CSV intake workflows, allowing the client to load large amounts of data into DocuSign CLM via SFTP or API, with automated processes including validation, file replacement, and scheduled archiving. I implemented automated error handling including logs, authentication and request retries, and emails to the appropriate parties.</p></li>
+                                <li className='mb-2' class="lightBlue"><p class="lightBlue">Working on projects for the second largest communications company in the world, I developed integrations that improved efficiency in HR, procurement, and reporting. I built complex workflows in DocuSign CLM that communcate with various external systems via API. For example, if an employee is transfered to a new department, the source system sends an API request, with the employee data in the body, to DocuSign CLM, which kicks off a workflow. This workflow validates the data, sends status updates to back to the source system, determines if the employee has been transferred, newly hired, or terminated, among other things. Based on a number of conditionals, the workflow follows the appropriate path and the employee file is updated and moved to the correct root folder.</p></li>
+                                <li className='mb-2' class="lightBlue"><p class="lightBlue">I developed integrations via API between systems such as Salesforce, NetSuite, PEGA, and others, for companies across a variety of industries using middleware including Jitterbit, Mulesoft, and Boomi. Often using scheduled requests to extract data from various sources, transforming the data and making a request to the target system to load that data.  </p></li>
+                           </ul>
+                           </div>
+
+                    </Col>
+
+                    <Col className="col-12 col-md-5 mb-5 pl-4 p-3">
+                    <h6 className="section-title mt-4">DocuSign CLM Developer Capstone</h6>
                        <div className="images mt-3">
                             <img className="tidyFamHome" src="../../images/DocuSignWF2.png" height="322rem" width="100%" alt="Screenshot of Devon Cox DocuSign capstone" />
                         </div>
                         
                         <p>
-                            <p className="lightBlue pt-2">I became a Certified DocuSign CLM Developer early in 2022. My capstone project covers an integration with Salesforce. Over the next 18 months, as I worked with clients, my workflows became much more complex and included custom integrations with various systems. View the presentation <a href="https://youtu.be/3rRcF0Ci-Tw">here</a>. </p>
+                            <p className="lightBlue pt-4">I became a Certified DocuSign CLM Developer early in 2022. My capstone project covers an integration with Salesforce. Over the next 18 months, as I worked with clients, my workflows became much more complex and included custom integrations with various systems. View the presentation <a href="https://youtu.be/3rRcF0Ci-Tw">here</a>. </p>
                             <p className="lightBlue">Throughout this time, I also developed using other platforms and became a certified Jitterbit Developer. </p>
                         </p>
-
-                    </Col>
-
-                    <Col className="col-12 col-md-5 mb-5 pl-4 p-3">
-                    <h6 className="section-title mt-4">Integrations Consultant/Developer</h6> 
-                       
+                        <h6 className="section-title mt-4">Website Design and Development</h6>
                        <div className="images mt-3">
-                      
-                           <ul>
-                               <li className='mb-2' class="lightBlue"><p class="lightBlue">I worked on a team helping to automate and improve procurement processes for one of the world's largest snack companies, through an integration with their homegrown systems and DocuSign CLM. Acions in the source system trigger an API request, which kicks off workflows in DocuSign CLM. These workflows receive data from the source system via the API request, then validate and prepare that data for various uses within DocuSign CLM, including contracting and reporting. Status updates and other data are sent back to the source system via API requests. I also designed and developed CSV intake workflows, allowing the client to load large amounts of data into DocuSign CLM via SFTP or API, with automated processes including validation, file replacement, and scheduled archiving. I implemented automated error handling including logs, authentication and request retries, and emails to the appropriate parties.</p></li>
-                                <li className='mb-2' class="lightBlue"><p class="lightBlue">Working on projects for the second largest communications company in the world, I developed integrations that improved efficiency in HR, procurement, and reporting. I built complex workflows in DocuSign CLM that communcate with various external systems via API. For example, if an employee is transfered to a new department, the source system sends an API request, with the employee data in the body, to DocuSign CLM, which kicks off a workflow. This workflow validates the data, sends status updates to back to the source system, determines if the employee has been transferred, newly hired, or terminated, among other things. Based on a number of conditionals, the workflow follows the appropriate path and the employee file is updated and moved to the correct root folder.</p></li>
-                                <li className='mb-2' class="lightBlue"><p class="lightBlue">I developed integrations via API between systems such as Salesforce, NetSuite, PEGA, and others, for companies across a variety of industries using middleware including Jitterbit, Mulesoft, and Boomi. Often using scheduled requests to extract data from various sources, transforming the data and making a request to the target system to load that data.  </p></li>
-                           </ul>
-                       </div>
+                            <img className="tidyFamHome" src="../../images/LFP.png" height="322rem" width="100%" alt="Screenshot of Lakeway Financial Planning Website" />
+                        </div>
+                        
+                        <p>
+                            <p className="lightBlue pt-4">I designed and developed my first WordPress website in late 2022. I worked with my client to design the site and used Elementor along with custom CSS, HTML, and PHP code to build it. View the live site <a href="https://lakewayfinancialplanning.com/">here</a>. </p>
+                            <p className="lightBlue">Throughout this time, I also developed using other platforms and became a certified Jitterbit Developer. </p>
+                        </p>
+                    
+                    
 
                     </Col>
                     </Row>
