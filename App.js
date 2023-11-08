66498cdf81df0baf04f161219dcbd16e0e0c348d
Devco2011 <devco2011@gmail.com>
2023-11-08 12:45:38 -0600

    moved app

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Route } from "react-router-dom";
-import { ApplicationViews } from "./ApplicationViews.js";
+import { ApplicationViews } from "./src/ApplicationViews.js";
 
 export const App = () => {
   return (
